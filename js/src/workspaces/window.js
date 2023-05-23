708450c1ab6e7a170533ab167adf573b5295113b
berger-n <quai.ledrurollin@gmail.com>
2023-05-23 18:05:33 +0200

    open/close feedbucket from mirador (buda-base/public-digital-library#804)

--- js/src/workspaces/window.js
+++ js/src/workspaces/window.js
@@ -189,7 +189,7 @@ var Z = 0 ;
       templateData.showFullScreen = _this.fullScreen;
       templateData.userButtons = _this.userButtons;
 
-      //templateData.useFeedbucket = window.useFeedbucket;
+      templateData.useFeedbucket = window.useFeedbucket;
 
       if(this.qualityGrade == "0") templateData.quality0 = true;
 
@@ -281,6 +281,15 @@ var Z = 0 ;
         jQuery(".view-nav #Zmenu ul.select").append("<li "+ /*(i === 0?"data-selected='true'":"")+*/ " data-value='"+(i)+"'>"+(/*i === 0?"auto":*/(i)+"%")+"</li>") ;            
       }  
             
+      if(window.useFeedbucket) {
+        jQuery("feedbucket-app").removeClass("on");
+        jQuery(".mirador-container #feedbucket").click(function(event){
+          var elem = jQuery(event.currentTarget);
+          elem.toggleClass("X");
+          jQuery("feedbucket-app").toggleClass("on");
+        });
+      }
+
       jQuery(".view-nav #Zmenu ul.select li").click(function(event){
         var elem = jQuery(event.currentTarget).closest("li");
         if(elem.attr("data-value")) {
@@ -1500,7 +1509,7 @@ var Z = 0 ;
       '<div class="overlay"></div>',
       '<div class="view-container {{#if inApp}}inApp thumbs-off{{/if}} {{#unless sidePanel}}focus-max-width{{/unless}}">',
       
-      //'{{#if useFeedbucket}}<div id="feedbucket"></div>{{/if}}',
+      '{{#if useFeedbucket}}<div id="feedbucket"><svg class="MuiSvgIcon-root-1" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path fill="none" d="M0 0h24v24H0z"></path><path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 12h-2v-2h2v2zm0-4h-2V6h2v4z"></path></svg><svg class="MuiSvgIcon-root-1" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path><path fill="none" d="M0 0h24v24H0z"></path></svg></div>{{/if}}',
 
       // TODO move to mirador-main-menu
       //'{{#if inApp}}<div class="mobile-button top"><img src="/icons/burger.svg"/></div>{{/if}}',
